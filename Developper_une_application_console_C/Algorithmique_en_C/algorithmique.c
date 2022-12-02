fd013b19cf94a96328786f9c25d14a834e8e4231
AnthoninB70 <109092404+AnthoninB70@users.noreply.github.com>
2022-12-02 14:51:35 +0100

    Create algorithmique.c

---
+++ Developper_une_application_console_C/Algorithmique_en_C/algorithmique.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main() {
+  int age;
+  int argent;
+  printf("Quel âge avez-vous ?\n");
+  scanf("%d", & age);
+  printf("Combien d'agents avez-vous ?\n");
+  scanf("%d", & argent);
+  if (age <= 10 && argent <= 1000) {
+    printf("Hors de ma vue jeune misérable\n");
+  } else if (age <= 10 && argent >= 1001 && argent <= 10000) {
+    printf("Salut jeune\n");
+  } else if (age <= 10 && argent >= 10001 && argent <= 100000) {
+    printf("Bienvenue jeune Picsou\n");
+  }
+  if (age >= 11 && age <= 20 && argent <= 1000) {
+    printf("Hors de ma vue, adolescent misérable\n");
+  } else if (age >= 11 && age <= 20 && argent >= 1001 && argent <= 10000) {
+    printf("Salut adolescent\n");
+  } else if (age >= 11 && age <= 20 && argent >= 10001 && argent <= 100000) {
+    printf("Bienvenue, adolescent Picsou\n");
+  }
+  if (age >= 21 && age <= 30 && argent <= 1000) {
+    printf("Hors de ma vue jeune adulte misérable\n");
+  } else if (age >= 21 && age <= 30 && argent >= 1001 && argent <= 10000) {
+    printf("Salut jeune adulte\n");
+  } else if (age >= 21 && age <= 30 && argent <= 10001 && argent <= 100000) {
+    printf("Bienvenue jeune adulte Picsou\n");
+  }
+  if (age >= 31 && age <= 40 && argent <= 1000) {
+    printf("Hors de ma vue vieux misérable\n");
+  } else if (age >= 31 && age <= 40 && argent >= 1001 && argent <= 10000) {
+    printf("Salut vieux\n");
+  } else if (age >= 31 && age <= 40 && argent >= 10001 && argent <= 100000) {
+    printf("Bienvenue vieux Picsou\n");
+  }
+  return 0;
+}

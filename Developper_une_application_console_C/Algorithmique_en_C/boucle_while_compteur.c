a3d7f3b9ddc86c8e648ccd14450cd01d6735d235
AnthoninB70 <109092404+AnthoninB70@users.noreply.github.com>
2022-12-05 16:46:05 +0100

    Create boucle_while_compteur.c

---
+++ Developper_une_application_console_C/Algorithmique_en_C/boucle_while_compteur.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main() {
+  int compteur = 0;
+  while (compteur < 10) {
+    compteur = compteur + 1;
+    printf("%i\n", compteur);
+  }
+  return 0;
+}

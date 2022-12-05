0d3c5c2697d39dfbc79f1ff301f51495b9ec8662
AnthoninB70 <109092404+AnthoninB70@users.noreply.github.com>
2022-12-05 16:45:34 +0100

    Create boucle_for_compteur.c

---
+++ Developper_une_application_console_C/Algorithmique_en_C/boucle_for_compteur.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main() {
+  int compteur;
+  for (compteur = 0; compteur < 11; compteur = compteur + 1) {
+    printf("%i\n", compteur);
+  }
+  return 0;
+}
